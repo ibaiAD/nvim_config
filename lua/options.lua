477e2c60e6b624b05b7886146df445978970e600
ibaiAD <ibaiazcona@gmail.com>
2026-08-29 18:02:06 +0200

    disable line wrap

--- lua/options.lua
+++ lua/options.lua
@@ -9,7 +9,7 @@ vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
-vim.opt.wrap = true -- line wrap
+vim.opt.wrap = false -- line wrap
 
 -- not vim backups, undotree long runing undos
 vim.opt.swapfile = false
